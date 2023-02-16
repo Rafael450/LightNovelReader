b9be31db6afb4a300ebd3eabea7c334984f0abc6
Rafael450 <rafaelgamer450@gmail.com>
2023-02-15 23:30:50 -0300

    feat: created function that tests if phrase image is the beginning of the text.

--- picture.py
+++ picture.py
@@ -1,9 +1,12 @@
-from PIL import Image, ImageGrab
+import imaplib
+from PIL import ImageOps, ImageGrab, Image
+import numpy as np
 import time, pyautogui
 
-def takePictures(amount, box=(343,105,2557,1315)):
+def take_pictures(amount, box=(343,105,2557,1315)):
     print("Taking {} screenshots in 5 seconds".format(amount))
     time.sleep(5)
+
     for i in range(amount):
         print("Picture " + str(i))
         img = ImageGrab.grab(bbox=box)
@@ -11,24 +14,36 @@ def takePictures(amount, box=(343,105,2557,1315)):
         img.save(save_path)
         pyautogui.press('left')
 
-def cropPicture(picture, columns=18, newbox=(104,89,2079,1126)):
+def crop_picture(picture, columns=18, newbox=(104,89,2079,1126)):
     fit_img = picture.crop(newbox)
     phrases = []
     width, height = fit_img.size
     box_size = width/columns
+
     for i in range(columns):
         phrase_new = fit_img.crop((width-(i+1)*box_size, 0, width-(i)*box_size, height))
         phrases.append(phrase_new)
-        save_path = ".\phrase_test\\{}.png".format(i)
-        phrase_new.save(save_path)
+        # save_path = ".\phrase_test\\{}.png".format(i)
+        # phrase_new.save(save_path)
+    
+    return phrases
+
+def is_first_line(phrase, height=84):
+    width, _ = phrase.size
+    img_top = ImageOps.grayscale(phrase.crop((0,0,width,height)))
+    blackness = 255-np.average(img_top)
+    print(blackness)
+    return True if blackness < 0.5 else False
+
+
     
     
 
 
-
-
+for i in range(17):
+    print(is_first_line(Image.open('.\\phrase_test\\{}.png'.format(i))))
 # takePictures(35)
-cropPicture(Image.open('.\\snapshots\\30.png'))
+# cropPicture(Image.open('.\\snapshots\\30.png'))
 
 
         
